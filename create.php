fd154225fe66be86d680b358e7ee22becc98b3bf
VisalTrano <31500833+VisalTrano@users.noreply.github.com>
2019-03-19 17:52:24 +0700

    change code upload photo

--- create.php
+++ create.php
@@ -6,9 +6,9 @@
  * Time: 11:20 PM
  */
 include "conn.php";
-//check name already exit if exit == true
+//check name and birthdate already exit if exit == true
 function checkName($name,$conn){
-    $sql = "SELECT *FROM student WHERE studentName='".$name."'";
+    $sql = "SELECT *FROM student WHERE studentName='".$name."' and bithDate='".$_POST['birthDate']."'";
     $result = mysqli_query($conn, $sql);
     if(mysqli_num_rows($result)>0)
         return true;
@@ -17,33 +17,20 @@ function checkName($name,$conn){
 }
 
 //upload image to dir photos
-function uploadPhoto($name){
+function uploadPhoto(){
     $oldpath = $_FILES['photo']['tmp_name'];
-    $newpath ="photos/".$name.".jpg";
+    $newpath ="photos/".$_FILES['photo']['name'];
     move_uploaded_file($oldpath, $newpath);
 }
 
-//getlast studentId
-function getLastId($conn ){
-    $sql = "SELECT *FROM student ORDER BY studentId DESC";
-    $result = mysqli_query($conn, $sql);
-    if(mysqli_num_rows($result)>0) {
-        $row = mysqli_fetch_assoc($result);
-        return $row['studentId'];
-    }
-    else
-       return 0;
-}
 
 //Insert Data to database
 function addData($conn){
-    $lastId=getLastId($conn);
-    $photoName=$_POST['studentName'].($lastId+1);
     $sql="INSERT INTO student (studentName, bithDate,address,photoName)
-    VALUES ('".$_POST['studentName']."', '".$_POST['birthDate']."', '".$_POST['address']."','photos/".$photoName."')";
+    VALUES ('".$_POST['studentName']."', '".$_POST['birthDate']."', '".$_POST['address']."','photos/".$_FILES['photo']['name']."')";
     if (mysqli_query($conn, $sql)) {
         echo "<h3>Create Student Successfully</h3>";
-      uploadPhoto($photoName);
+      uploadPhoto();
     } else {
         echo "Error: " . $sql . "<br>" . mysqli_error($conn);
     }
